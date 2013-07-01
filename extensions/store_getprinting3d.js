097d6847855234d592b7174ce396a89f7c630dff
Kim Horakh <kimh@zoovy.com>
2013-07-01 15:35:10 -0700

    Applied jcycle to banners

--- extensions/store_getprinting3d.js
+++ extensions/store_getprinting3d.js
@@ -34,7 +34,14 @@ var store_getprinting3d = function() {
 		init : {
 			onSuccess : function()	{
 				var r = false; //return false if extension won't load for some reason (account config, dependencies, etc).
-
+				
+				app.rq.push(['templateFunction','homepageTemplate','onCompletes',function(P) {
+					//run slideshow code
+					var $context = $(app.u.jqSelector('#',P.parentID));
+					
+					$('.mainBannerGraphic', $context).cycle();
+					}]);
+				
 				//if there is any functionality required for this extension to load, put it here. such as a check for async google, the FB object, etc. return false if dependencies are not present. don't check for other extensions.
 				r = true;
 
