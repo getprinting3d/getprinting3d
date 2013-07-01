481a4e4d1c473344d036e74362562d66ca81584d
Kim Horakh <kimh@zoovy.com>
2013-07-01 15:08:17 -0700

    load jcycle

--- app-fashion-init.js
+++ app-fashion-init.js
@@ -19,6 +19,7 @@ app.rq.push(['extension',0,'store_crm','extensions/store_crm.js']);
 app.rq.push(['extension',0,'myRIA','app-quickstart.js','startMyProgram']);
 app.rq.push(['extension',0,'store_getprinting3d','extensions/store_getprinting3d.js']);
 app.rq.push(['extension',0,'tools_lightbox','extensions/tools_lightbox/tools_lightbox.js']);
+app.rq.push(['script',0,app.vars.baseURL+'extensions/jquery-cycle.js']);
 
 //app.rq.push(['extension',1,'google_analytics','extensions/partner_google_analytics.js','startExtension']);
 //app.rq.push(['extension',0,'partner_addthis','extensions/partner_addthis.js','startExtension']);
